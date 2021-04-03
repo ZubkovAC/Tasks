04d38e0d843dfdb881998fd123416aadcadbcac1
djan2003 <nikiforov777@gmail.com>
2021-04-03 12:49:08 +0300

    create new branch

--- src/n1-main/m1-UI/Common/Registration/Registration.tsx
+++ src/n1-main/m1-UI/Common/Registration/Registration.tsx
@@ -14,6 +14,7 @@ export const Registration = () => {
             <h3>Your email</h3>
             <SuperInputText/>
             <SuperButton title={'create'}/>
+            <p>Check that branch was created</p>
         </div>
     )
 }
