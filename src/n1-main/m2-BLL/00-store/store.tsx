1bada2dd041113d2fbaac6b2121c080c3a86a5a9
nickolayArbuzov <56513866+nickolayArbuzov@users.noreply.github.com>
2021-04-08 02:07:44 +0300

    feat: add login-reducer

--- src/n1-main/m2-BLL/00-store/store.tsx
+++ src/n1-main/m2-BLL/00-store/store.tsx
@@ -2,14 +2,15 @@ import {applyMiddleware,combineReducers, createStore} from "redux";
 import {StartReducer} from "../01-reduser1/start";
 import {reserseReducer} from "../03-reducer-newPassword/reverseRassword";
 import {registrationReducer} from "../01-reduser1/registration-reducer";
+import {loginReducer} from "../02-reducer-login/login";
 import thunk from 'redux-thunk'
 
 let rootReducer = combineReducers({
     start:StartReducer,
     newPassword:reserseReducer,
     registration: registrationReducer,
+    login: loginReducer
 })
-
 
 export let store = createStore(rootReducer,applyMiddleware(thunk))
 
