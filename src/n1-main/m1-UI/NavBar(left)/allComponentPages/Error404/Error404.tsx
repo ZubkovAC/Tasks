9e0c4dbb6652efaeca42d4dd5345666dd5e034b1
ZubkovAC <3y6kob90@gmail.com>
2021-05-20 14:23:18 +0300

    tasks 4.27(error font + bgc)

--- src/n1-main/m1-UI/NavBar(left)/allComponentPages/Error404/Error404.tsx
+++ src/n1-main/m1-UI/NavBar(left)/allComponentPages/Error404/Error404.tsx
@@ -5,17 +5,16 @@ export const Error404 = () =>{
     return (
         <div className={css.App}>
             <div className={css.space}>
-                Er<span className={css.R}>r</span>o<span>r</span>
+                Er<span className={css.R}>r</span>o
+                <span className={css.R}>r</span>
             </div>
-            <div>
+            <div className={css.R}>
                 <span className={css.off}>4</span>0
-                <div id='hangEdge'>
-                    <span>4</span>
+                <div className={css.hangEdge}>
+                    <span className={css.R} >4</span>
                 </div>
             </div>
         </div>
     )
 }
 
-
-// http://localhost:7542/2.0/?frontTime=1596635884283
